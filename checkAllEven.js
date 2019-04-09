1740442767b553d5660cb8785d33940eb309b09c
Tina <radostinadimitrova@gmail.com>
2019-04-09 11:35:10 +0300

    Added checkAllEven file

---
+++ checkAllEven.js
@@ -0,0 +1,6 @@
+// Fix this incorrect code!
+function checkAllEven(arr) {
+  return arr.every(x => x % 2 === 0);
+}
+console.log(checkAllEven([1, 2, 3, 4]));
+console.log(checkAllEven([8, 2, 6, 4]));
