af7a876ffaa6f206e407d047a0c3421ea59ed7c9
Tina <radostinadimitrova@gmail.com>
2019-04-11 13:17:52 +0300

    feat: Solve filterArray challenge

---
+++ js/filterArray.js
@@ -0,0 +1,6 @@
+function filterArray(arr) {
+	return arr.filter(el => typeof el === 'number');
+}
+console.log(filterArray([1, 2, "a", "b"]));
+console.log(filterArray([1, "a", "b", 0, 15]));
+console.log(filterArray([1, 2, "aasf", "1", "123", 123]));
