404f190a6c9d718c03a8a28c7e03d55ddbbe3336
Tina <radostinadimitrova@gmail.com>
2019-04-14 16:40:56 +0300

    feat: Solve isStrangePair challenge

---
+++ js/Easy_VeryEasy/isStrangePair.js
@@ -0,0 +1,6 @@
+function isStrangePair(str1, str2) {
+  
+    return [...str1][0] === [...str2][str2.length-1] && [...str1][str1.length-1] === [...str2][0] ?
+    true : false;
+  }
+  console.log(isStrangePair("ratio", "orator"));//true
