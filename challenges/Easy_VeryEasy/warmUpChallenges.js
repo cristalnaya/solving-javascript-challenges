96b59b7154e5ad4f00147302346871dc1d2ba0c6
Tina <radostinadimitrova@gmail.com>
2019-04-22 18:53:18 +0300

    feat: Solve more warmUp challenges

--- challenges/Easy_VeryEasy/warmUpChallenges.js
+++ challenges/Easy_VeryEasy/warmUpChallenges.js
@@ -15,3 +15,35 @@ function concat(arr1, arr2) {
   }
   console.log(divisibleByFive(5)); //true
   console.log(divisibleByFive(37)); //false
+
+  function isEmpty(s) {
+    return !s || s === null;
+  }
+  console.log(isEmpty('')); //true
+  console.log(isEmpty(' ')); //false
+
+  function animals(chickens, cows, pigs) {
+    return chickens * 2 + cows * 4 + pigs * 4;
+  }
+  console.log(animals(2, 3, 5)); //36
+
+  // solution 1
+  function differenceMaxMin(arr) {
+    // solution with spread syntax
+    return Math.max(...arr) - Math.min(...arr);
+  };
+  console.log(differenceMaxMin([10, 4, 1, 4, -10, -50, 32, 21]));//82
+
+  // solution 2
+  function differenceMaxMin2(arr) {
+    let sortedArr = arr.sort((a, b) => a - b);
+      return sortedArr[sortedArr.length-1] - sortedArr[0];
+  };
+  console.log(differenceMaxMin2([10, 4, 1, 4, -10, -50, 32, 21]));//82
+
+  function nextElement(arr) {
+    let difference = arr[arr.length - 1] - arr[arr.length-2];
+
+    return arr[arr.length - 1] + difference;
+  }
+  console.log(nextElement([3, 5, 7, 9])); // 11
